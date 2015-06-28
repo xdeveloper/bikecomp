5046394bf212cafad81dcf7d7326a35644356d53
xdeveloper <alexander.abakumov@gmail.com>
2015-06-28 12:54:37 +0300

    Move to Gradle
    Signed-off-by: xdeveloper <alexander.abakumov@gmail.com>

---
+++ app/src/main/java/ua/com/abakumov/bikecomp/MainActivity.java
@@ -0,0 +1,60 @@
+package ua.com.abakumov.bikecomp;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import ua.com.abakumov.bikecomp.fragment.AverageSpeedFragment;
+import ua.com.abakumov.bikecomp.fragment.SpeedFragment;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addFragments();
+    }
+
+    private void addFragments() {
+        // Add some fragments
+        Fragment speedFragment = new SpeedFragment();
+        Fragment averageSpeedFragment = new AverageSpeedFragment();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.mainLayout, speedFragment)
+                .add(R.id.mainLayout, averageSpeedFragment)
+                .show(speedFragment)
+                .show(averageSpeedFragment)
+                .commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- gen/ua/com/abakumov/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package ua.com.abakumov;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
