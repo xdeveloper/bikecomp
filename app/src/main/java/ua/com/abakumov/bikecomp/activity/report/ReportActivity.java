27fdda33a74beb019f1f16f4131c10fd42b240dc
alexander.abakumov <alexander.abakumov@gmail.com>
2016-03-20 20:45:13 +0200

    Google Fit. Not working at all.

--- app/src/main/java/ua/com/abakumov/bikecomp/activity/report/ReportActivity.java
+++ app/src/main/java/ua/com/abakumov/bikecomp/activity/report/ReportActivity.java
@@ -1,13 +1,16 @@
 package ua.com.abakumov.bikecomp.activity.report;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -23,6 +26,7 @@ import static ua.com.abakumov.bikecomp.util.helper.Helper.formatDate;
 import static ua.com.abakumov.bikecomp.util.helper.Helper.formatElapsedTime;
 import static ua.com.abakumov.bikecomp.util.helper.Helper.formatSpeed;
 import static ua.com.abakumov.bikecomp.util.helper.Helper.formatTime;
+import static ua.com.abakumov.bikecomp.util.helper.LogHelper.information;
 
 import com.facebook.FacebookActivity;
 import com.facebook.FacebookDialog;
@@ -31,6 +35,23 @@ import com.facebook.share.model.ShareContent;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.model.ShareOpenGraphContent;
 import com.facebook.share.widget.ShareDialog;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.internal.zzj;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -40,7 +61,11 @@ import com.facebook.share.widget.ShareDialog;
  */
 public class ReportActivity extends AppCompatActivity {
 
+    private static final String TAG = "dsdsd";
+    private static final int SOME_FUCKING_MAGIC_NUMBER = 42;
     private Ride ride;
+
+    private GoogleApiClient mClient = null;
 
     // ----------- System --------------------------------------------------------------------------
 
@@ -52,11 +77,27 @@ public class ReportActivity extends AppCompatActivity {
 
         ride = getIntent().getParcelableExtra(Ride.class.getCanonicalName());
 
+        /// ---------------- google fit -------------
+        // This method sets up our custom logger, which will print all log messages to the device
+        // screen, as well as to adb logcat.
+        buildFitnessClient();
+
+        // When permissions are revoked the app is restarted so onCreate is sufficient to check for
+        // permissions core to the Activity's functionality.
+        //if (!checkPermissions()) {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_CONTACTS},
+                SOME_FUCKING_MAGIC_NUMBER);
+        //}
+
+
         saveSession(ride);
+
 
         setContentView(R.layout.activity_report);
 
-        findViewById(R.id.shareButton).setOnClickListener(v -> {
+        /*findViewById(R.id.shareButton).setOnClickListener(v -> {
             FacebookSdk.sdkInitialize(this);
             ShareLinkContent linkContent = new ShareLinkContent.Builder()
                     .setContentTitle("Hello Facebook")
@@ -66,10 +107,7 @@ public class ReportActivity extends AppCompatActivity {
                     .build();
 
 
-
             ShareDialog.show(this, linkContent);
-
-
         });
 
         findViewById(R.id.activityReportExitButton).setOnClickListener(v -> {
@@ -81,10 +119,10 @@ public class ReportActivity extends AppCompatActivity {
             });
             dialog.findViewById(R.id.cancel_button_dlg).setOnClickListener(v2 -> dialog.dismiss());
             dialog.show();
-        });
-
+        });*/
 
     }
+
 
     @Override
     protected void onStart() {
@@ -108,6 +146,10 @@ public class ReportActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
+        // This ensures that if the user denies the permissions then uses Settings to re-enable
+        // them, the app will start working.
+        buildFitnessClient();
+
         super.onResume();
     }
 
@@ -155,6 +197,53 @@ public class ReportActivity extends AppCompatActivity {
     private void saveSession(Ride ride) {
         DBHelper dbHelper = new DBHelper(this);
         dbHelper.save(ride);
+
+
+        // Set a start and end time for our data, using a start time of 1 hour before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.HOUR_OF_DAY, -1);
+        long startTime = cal.getTimeInMillis();
+
+        // Create a data source
+        DataSource dataSource = new DataSource.Builder()
+                .setAppPackageName(this)
+                .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .setStreamName("TAG" + " - step count")
+                .setType(DataSource.TYPE_RAW)
+                .build();
+
+// Create a data set
+        int stepCountDelta = 950;
+        DataSet dataSet = DataSet.create(dataSource);
+// For each data point, specify a start time, end time, and the data value -- in this case,
+// the number of new steps.
+        DataPoint dataPoint = dataSet.createDataPoint()
+                .setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS);
+        dataPoint.getValue(Field.FIELD_STEPS).setInt(stepCountDelta);
+        dataSet.add(dataPoint);
+
+
+        // Then, invoke the History API to insert the data and await the result, which is
+// possible here because of the {@link AsyncTask}. Always include a timeout when calling
+// await() to prevent hanging that can occur from the service being shutdown because
+// of low memory or other conditions.
+        // Log.i(TAG, "Inserting the dataset in the History API.");
+
+        //
+        Fitness.HistoryApi.insertData(mClient, dataSet).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                information("!");
+            }
+        });
+
+
+// Before querying the data, check to see if the insertion succeeded.
+
+
     }
 
     private void quitApplication() {
@@ -165,4 +254,58 @@ public class ReportActivity extends AppCompatActivity {
         startActivity(mainIntent);
     }
 
+
+    private void buildFitnessClient() {
+        if (mClient == null && checkPermissions()) {
+            mClient = new GoogleApiClient.Builder(this)
+                    .addApi(Fitness.HISTORY_API)
+                    .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                    .addConnectionCallbacks(
+                            new GoogleApiClient.ConnectionCallbacks() {
+                                @Override
+                                public void onConnected(Bundle bundle) {
+                                    Log.i(TAG, "Connected!!!");
+                                    // Now you can make calls to the Fitness APIs.
+                                    findFitnessDataSources();
+                                }
+
+                                @Override
+                                public void onConnectionSuspended(int i) {
+                                    // If your connection to the sensor gets lost at some point,
+                                    // you'll be able to determine the reason and react to it here.
+                                    if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                        Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+                                    } else if (i
+                                            == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                        Log.i(TAG,
+                                                "Connection lost.  Reason: Service Disconnected");
+                                    }
+                                }
+                            }
+                    )
+                    .enableAutoManage(this, 0, new GoogleApiClient.OnConnectionFailedListener() {
+                        @Override
+                        public void onConnectionFailed(ConnectionResult result) {
+                            /*Log.i(TAG, "Google Play services connection failed. Cause: " +
+                                    result.toString());
+                            Snackbar.make(
+                                    MainActivity.this.findViewById(R.id.main_activity_view),
+                                    "Exception while connecting to Google Play services: " +
+                                            result.getErrorMessage(),
+                                    Snackbar.LENGTH_INDEFINITE).show();*/
+                        }
+                    })
+                    .build();
+        }
+
+    }
+
+    private void findFitnessDataSources() {
+
+    }
+
+    private boolean checkPermissions() {
+
+        return true;
+    }
 }
